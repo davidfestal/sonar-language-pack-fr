6055f6e73d15c6dbc6ae8d84f0fb9c24cef69570
David Festal <david.festal@serli.com>
2011-06-17 11:23:43 +0200

    Change List<> to Collection<> in the LanguagePack-implementation class to correct AbstractMethodError

--- core-language-pack-fr/src/main/java/org/sonar/i18n/core/fr/CoreFrLanguagePack.java
+++ core-language-pack-fr/src/main/java/org/sonar/i18n/core/fr/CoreFrLanguagePack.java
@@ -1,7 +1,7 @@
 package org.sonar.i18n.core.fr;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 import java.util.Locale;
 
 /**
@@ -9,11 +9,11 @@ import java.util.Locale;
  */
 public class CoreFrLanguagePack extends org.sonar.api.i18n.LanguagePack {
 
-  public List<String> getPluginKeys() {
+  public Collection<String> getPluginKeys() {
     return Arrays.asList("core", "design");
   }
 
-  public List<Locale> getLocales() {
+  public Collection<Locale> getLocales() {
     return Arrays.asList(Locale.FRENCH);
   }
 }
